f4d59f5105d329813249151098c9b6dfe665f986
Suryansh Dey <deysuryansh@gmail.com>
2024-10-31 12:42:15 +0530

    added newline after print1D()

--- src/others.cpp
+++ src/others.cpp
@@ -5,12 +5,14 @@ void MyStl::print1D(type array1D) noexcept
 {
     for (auto element : array1D)
         std::cout << element << ' ';
+    std::cout << '\n';
 }
 template <typename type>
 void MyStl::print1D(type array1D, size_t size) noexcept
 {
     for (size_t i = 0; i < size; i++)
         std::cout << array1D[i] << ' ';
+    std::cout << '\n';
 }
 std::pair<uint32_t, uint32_t> MyStl::fileFirstDifference(const char *outputFilePath, const char *testFilePath)
 {
